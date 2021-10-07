7538b1aee6bbf0b0c9c26bb3ade33376633713a4
mochammad irzan <m.irzan@gmail.com>
2021-10-07 22:28:42 +0700

    latest

---
+++ golang/test3.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+    var ( 
+        data1, data2 float64 = 15.1111,25.333
+        data3 float64
+    )
+    fmt.Println("hello world")
+    fmt.Println("data1 ",data1)
+    fmt.Println("data2 ",data2)
+    data3 = data1 / data2
+    fmt.Println("data3 ",data3)
+    data1,data4 =  55.5,11.333
+    fmt.Println("data1 ",data1)
+    fmt.Println("data4 ",data4)
+}
