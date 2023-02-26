5db67d05c60c653a2db146617a3762f8a9e073bf
m1r24n <m.irzan@gmail.com>
2023-02-26 09:53:49 +0700

    update

---
+++ cpp/test7.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+int main(){
+		std::string name1="mochammad irzan";
+		std::cout << "value of name1 is  " << name1 << "\n";
+		name1="ratih dyah kusumastuti";	
+		std::cout << "value of name1 is  " << name1 << "\n";
+}
+
